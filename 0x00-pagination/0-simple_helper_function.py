9a9a9622e3cc32a1562473af33c09e58afcee0f5
emmanuel <emmanuellawakin2005@gmail.com>
2024-04-05 18:26:55 +0100

    Completed task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function that returns a tuple of size two
+containing a start index and an end index corresponding to the range of
+indexes to return a list for those particular pagination parameters
+"""
+
+
+def index_range(page, page_size):
+    """
+    This function returns a tuple containing a start index and an end
+    index corresponding to the range of indexes to return a list for
+    those particular pagination parameters
+    """
+    end_index = page * page_size
+    if page == 1:
+        start_index =  0
+    else:
+        start_index = (page - 1) * page_size
+    return (start_index, end_index)
